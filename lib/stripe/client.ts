7f17d1300d8e05d239ca3bba3ce3f9721c12ddca
elmo3159 <elmo.123912@gmail.com>
2025-11-16 20:16:44 +0900

    fix: Stripe環境変数のエラーハンドリングを改善

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -5,7 +5,13 @@ import Stripe from 'stripe'
  *
  * サーバーサイドでのみ使用
  */
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error(
+    'STRIPE_SECRET_KEY環境変数が設定されていません。Vercelダッシュボードで環境変数を設定してください。'
+  )
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2025-10-29.clover',
   typescript: true,
 })
